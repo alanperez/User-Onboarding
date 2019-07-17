3e3b3bb05127fc5fcc7a6227a78db30e78b56476
Alan Perez <alanp9six@gmail.com>
2019-07-17 13:57:12 -0500

    :sparkles: Created components folder and setup the BoardForm component just to display text inside a div

---
+++ user-onboard/src/components/BoardForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { withFormik, Form, Field } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
+
+
+function BoardForm() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+
+export default BoardForm
